3a8f6917ce1501d44172c7a4fa5395931d279911
MalavikaAjit <malavikaajit9@gmail.com>
2021-09-09 02:25:09 +0530

    ADD:added api for signin

--- javascript/login-signin.js
+++ javascript/login-signin.js
@@ -1,49 +1,173 @@
+
+var err =false;
+function validateEmail1() {
+    const email = document.getElementById('email1');
+    const emailError = document.getElementById('email-error1');
+
+    let emailRegex = RegExp('^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$');
+    if (emailRegex.test(email.value)){
+    emailError.textContent = "";
+    err =true;
+    }else emailError.textContent = "sorry, your user name must be between 6 and 30 character long";    
+};
+
+function validateEmptyEmail1() { 
+    const email = document.getElementById('email1');
+    const emailError = document.getElementById('email-error1');
+ 
+    if(email.value == "") emailError.textContent = "choose a gmail address"; 
+    else { err = true;  }   
+};
+
+function validateEmptyPassword1() {  
+    const password1 = document.getElementById('password1');    
+    const passwordError1 = document.getElementById('password-error1');
+    if(password1.value == "") passwordError1.textContent = "Enter a password";  
+    else { err = true;  }
+};
+
+function validatePassword1 () {
+    const password1 = document.getElementById('password1');
+    const passwordError1 = document.getElementById('password-error1');
+    let passwordRegex1 = RegExp('^[a-zA-Z0-9]{8,}$');
+    if (passwordRegex1.test(password1.value)){
+    passwordError1.textContent = "";
+      err = true;
+    }else passwordError1.textContent = "enter valid password";        
+}
+
+/**************** */
 function validateEmail() {
     const email = document.getElementById('email');
     const emailError = document.getElementById('email-error');
 
     let emailRegex = RegExp('^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$');
-    if (emailRegex.test(email.value))
+    if (emailRegex.test(email.value)){
     emailError.textContent = "";
-    else emailError.textContent = "sorry, your user name must be between 6 and 30 character long";    
+    err=true;
+    }else emailError.textContent = "sorry, your user name must be between 6 and 30 character long";    
 };
 
 function validateEmptyEmail() { 
     const email = document.getElementById('email');
     const emailError = document.getElementById('email-error');
- 
-    if(email.value == "") emailError.textContent = "choose a gmail address";    
+    if(email.value == "") emailError.textContent = "choose a gmail address"; 
+    else { err = true;  }   
 };
 
 function validateEmptyPassword() {  
     const password = document.getElementById('password');    
     const passwordError = document.getElementById('password-error');
-    if(password.value == "") passwordError.textContent = "Enter a password";    
+    if(password.value == "") passwordError.textContent = "Enter a password";  
+    else { err = true;  }  
 };
 
 function validatePassword () {
     const password = document.getElementById('password');
     const passwordError = document.getElementById('password-error');
     let passwordRegex = RegExp('^[a-zA-Z0-9]{8,}$');
-    if (passwordRegex.test(password.value))
+    if (passwordRegex.test(password.value)){
     passwordError.textContent = "";
-    else passwordError.textContent = "enter valid password";        
+    err=true;
+    }else passwordError.textContent = "enter valid password";        
 }
-function validateName() {
-    const name = document.getElementById('full-name');
-   const nameError = document.getElementById('name-error');    
-
-    if(name.value == "") nameError.textContent = "Enter  name";        
+function validateName() { 
+    const name1 = document.getElementById('fullname');
+   const nameError = document.getElementById('name-error');   
+    if(name1.value == "") nameError.textContent = "Enter  name"; 
+    else { err = true;  }       
 }
+function validatePhone () {
+    const phone = document.getElementById('phone');
+    const phoneError = document.getElementById('phone-error');
+    let phoneRegex = RegExp('^[789]\d{9}$');
+    if (phoneRegex.test(phone.value)){
+    phoneError.textContent = "";
+    err=true;
+    }else phoneError.textContent = "enter valid phone no";        
+}
+function validateEmptyPhone() {  
+    const phone = document.getElementById('phone');
+    const phoneError = document.getElementById('phone-error');
+    if(phone.value == "") phoneError.textContent = "Enter a phone no"; 
+    else { err = true;  }   
+};
 
-function validator(page_name) {
-    console.log(page_name)
-    if(page_name==="index") {
+
+function validateLogin() {
+   
         validateEmail();
         validateEmptyEmail();
        validateEmptyPassword();
-       validatePassword();
+       validatePassword(); 
        
-    
-    }
 }
+
+function validateSignIn(){
+       
+    validateName()
+    validateEmail1();
+    validateEmptyEmail1();
+    validateEmptyPassword1();
+    // validatePassword1();  
+    // validatePhone ();
+    validateEmptyPhone(); 
+    
+    let data =
+    {
+        "fullName": fullname.value,
+        "email":email1.value,
+        "password":password1.value,
+        "phone": phone.value
+      }
+    //   const baseurl =  "https://new-bookstore-backend.herokuapp.com/bookstore_user/registration";
+    // axios.post(baseurl, data, headers)
+    postService('/bookstore_user/registration', data, headers)
+    .then(res=> {
+        console.log(res)
+        console.log(res.result._id)               
+            localStorage.setItem("token", res.result._id);
+})
+    
+    
+    // let data = {
+
+    // }
+    // postService('post', '/user/login', data)
+    //   .then(result => {
+
+    //     localStorage.setItem("token", result.id);
+    //     if (result.id) {
+    //     //   window.location.href = 'dashboard.html';
+    //       console.log("inside120")
+    //     }
+
+
+    //     return console.log('Success:', result);
+
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
+ }
+
+
+//  function loginCheck(inputArr) {
+//     inputArr.forEach(input => {
+//       if (err == true) {
+//         console.log(input, "required");
+//       }
+//       else {
+//         console.log('');
+//       }
+  
+//     });
+//   }
+//   function  validateSigninnn ()  {
+//     loginCheck([phone,password,email,name]); 
+//     if(err == true){
+//     console.log("yaay");
+//     }else{
+//         console.log("nope");
+//     }
+//   }
---
+++ service/service.js
@@ -0,0 +1,75 @@
+/*******service file for api get and post services******/
+
+
+const baseUrl = "https://new-bookstore-backend.herokuapp.com/";
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': localStorage.getItem('token')
+};
+
+// function getService(meth, url, data, headerss) {
+//     console.log(meth, baseUrl1 + url, data, headerss)
+//     return new Promise((resol, rej) => {
+//         fetch(baseUrl1 + url, {
+//             method: meth,
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': localStorage.getItem('token')
+//               }
+
+//         })
+//         .then(response => response.json())
+//         .then(result => {
+//             console.log(result)
+//              resol(result)
+
+//         })
+//         .catch((err)=>{
+//         console.log(err)
+//         })
+//     })
+// }
+
+
+// function postService(meth, url, data, headerss) {
+//     console.log(meth, baseUrl1 + url, data, headerss)
+//     return new Promise((resol, rej) => {
+//         fetch(baseUrl1 + url, {
+//             method: meth,
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': localStorage.getItem('token')
+//               },
+        
+//             body: JSON.stringify(data)
+//         })
+//         .then(response => response.json())
+//         .then(result => {
+//             console.log(result)
+//              resol(result)
+
+//         })
+//         .catch((err)=>{
+//                  console.log(err)
+//         })
+//     })
+// }
+
+function getService( url, headerss) {
+        // console.log( baseUrl1 + url, data, headerss)
+        return new Promise((resol, rej) => {
+            var resolved = axios.get(baseUrl + url, headerss);
+            resol(resolved);
+           })           
+        
+    }
+    
+    
+ function postService( url, data, headerss) {
+        // console.log(meth, baseUrl1 + url, data, headerss)
+        return new Promise((resol, rej) => {
+            var resolved = axios.get(baseUrl + url,data, headerss);
+            resol(resolved);
+        })
+    }
